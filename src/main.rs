3c440cb44338dafbf0187538fa9116ba31880928
ImonaMobilTeam <Rahim47@users.noreply.github.com>
2025-05-10 14:10:47 +0300

    fetch all users method implementation

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,25 @@
-mod services;
 mod models;
-//
-use actix_web::{get,post, web, App, HttpResponse, HttpServer, Responder,Error};
-use models::user_model::{CreateUserResponse,GetUserResponse};
-use models::user_model::User;
-//
-use std::sync::{Arc, Mutex};
-use std::collections::HashMap;
-//
+mod services;
+
 use crate::services::db::UserDb;
-//
-//
+use actix_web::{App, Error, HttpResponse, HttpServer, Responder, get, post, web};
+use models::user_model::User;
+use models::user_model::{CreateUserResponse, GetAllUsersResponse, GetUserResponse};
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+
 #[get("/greet")]
 async fn greet() -> impl Responder {
     HttpResponse::Ok().body("Hello, World!")
 }
-//
+
 #[get("/user/{id}")]
 async fn get_user(user_id: web::Path<i32>, db: web::Data<UserDb>) -> Result<impl Responder, Error> {
     let user_id = user_id.into_inner();
     let db = db.lock().unwrap();
     let user = db.get(&user_id);
     match user {
-        Some(user_data) => Ok(HttpResponse::Ok().json(GetUserResponse{
+        Some(user_data) => Ok(HttpResponse::Ok().json(GetUserResponse {
             id: user_id,
             name: user_data.name.clone(),
             email: user_data.email.clone(),
@@ -30,7 +27,22 @@ async fn get_user(user_id: web::Path<i32>, db: web::Data<UserDb>) -> Result<impl
         None => Ok(HttpResponse::NotFound().body("User not found")),
     }
 }
-//
+
+#[get("/users")]
+async fn get_all_users(db: web::Data<UserDb>) -> impl Responder {
+    let db = db.lock().unwrap();
+    let users: Vec<GetUserResponse> = db
+        .iter()
+        .map(|(&id, user)| GetUserResponse {
+            id,
+            name: user.name.clone(),
+            email: user.email.clone(),
+        })
+        .collect();
+
+    HttpResponse::Ok().json(GetAllUsersResponse { users })
+}
+
 #[post("/user")]
 async fn create_user(user_data: web::Json<User>, db: web::Data<UserDb>) -> impl Responder {
     let mut db = db.lock().unwrap();
@@ -44,24 +56,25 @@ async fn create_user(user_data: web::Json<User>, db: web::Data<UserDb>) -> impl 
         email,
     })
 }
-// git remote add origin https://github.com/Rahim47/simple-rust-web-server.git
-//
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-
-    let port: u16 = 8080; 
+    let port: u16 = 9999;
     println!("Server listening on port {}", port);
 
     let user_db: UserDb = Arc::new(Mutex::new(HashMap::<i32, User>::new()));
 
     HttpServer::new(move || {
         let app_data = web::Data::new(user_db.clone());
-        App::new().app_data(app_data).service(greet)
-        .service(get_user)
-        .service(create_user)
+        App::new()
+            .app_data(app_data)
+            .service(greet)
+            .service(get_user)
+            .service(get_all_users)
+            .service(create_user)
     })
-        .bind(("127.0.0.1", port))?
-        .workers(2)
-        .run()
-        .await
+    .bind(("127.0.0.1", port))?
+    .workers(2)
+    .run()
+    .await
 }
--- src/models/user_model.rs
+++ src/models/user_model.rs
@@ -1,12 +1,12 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Serialize, Deserialize)]
 pub struct User {
     pub name: String,
     pub email: String,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize)]
 pub struct CreateUserResponse {
     pub id: i32,
     pub name: String,
@@ -18,4 +18,9 @@ pub struct GetUserResponse {
     pub id: i32,
     pub name: String,
     pub email: String,
+}
+
+#[derive(Serialize)]
+pub struct GetAllUsersResponse {
+    pub users: Vec<GetUserResponse>,
 }
